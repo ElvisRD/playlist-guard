2f4f60e071102e65e05f0be3cdd7a3f89f603b92
ElvisRD <elvisappsdev@gmail.com>
2026-07-09 16:45:39 -0400

    feat: added playlists page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { Home } from './page/home/home';
-import { OauthCallback } from './page/oauth-callback/oauth-callback';
+import { Home } from './pages/home/home';
+import { OauthCallback } from './pages/oauth-callback/oauth-callback';
+import { Playlists } from './pages/playlists/playlists';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'auth/callback', component: OauthCallback },
+  { path: 'playlists', component: Playlists }
 ];
