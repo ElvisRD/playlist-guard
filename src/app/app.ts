7a1402d4b9d6249e98c757ea3958a6d9cb4a01a0
ElvisRD <elvisappsdev@gmail.com>
2026-07-20 18:02:41 -0400

    feat: added footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Navbar } from './components/navbar/navbar';
 import { RouterOutlet } from '@angular/router';
 import { Dialog } from './components/dialog/dialog';
 import { Toast } from './components/toast/toast';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Dialog, Toast],
+  imports: [RouterOutlet, Navbar, Dialog, Toast, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
